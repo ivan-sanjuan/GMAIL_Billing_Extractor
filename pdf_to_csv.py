4eb44dca359839380098602183c83df7eeefe6a6
vashmeow <isanjuanivan@gmail.com>
2025-10-24 06:43:09 +0800

    trying ocr for text encoding

--- pdf_to_csv.py
+++ pdf_to_csv.py
@@ -1,12 +1,16 @@
+from pdf_pass import pdf_pass
 import fitz
 import os
 
-folder_path = '\Projects\GMAIL_Billing_Extractor\pdf_files'
-
+folder_path = '\\Projects\GMAIL_Billing_Extractor\pdf_files'
+pdf_password = pdf_pass()
 for file_name in os.listdir(folder_path):
     file = os.path.join(folder_path,file_name)
 
     doc = fitz.open(file)
+    doc.authenticate(pdf_password)
     page = doc.load_page(3)            
-    text = page.get_text()
+    text = page.get_text('ocr')
+    # if not text.strip():
+    #     text = page.get_text('ocr')
     print(text)
