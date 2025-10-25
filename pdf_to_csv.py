b8684ec5932ef23bd407453d8e9c2095d578fb0d
vashmeow <isanjuanivan@gmail.com>
2025-10-25 22:31:30 +0800

    trying OCR to parse docs

--- pdf_to_csv.py
+++ pdf_to_csv.py
@@ -1,16 +1,19 @@
 from pdf_pass import pdf_pass
+import pytesseract
+from PIL import Image
+import io
 import fitz
 import os
 
 folder_path = '\\Projects\GMAIL_Billing_Extractor\pdf_files'
 pdf_password = pdf_pass()
-for file_name in os.listdir(folder_path):
-    file = os.path.join(folder_path,file_name)
-
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+for page in os.listdir(folder_path):
+    file = os.path.join(folder_path,page)
     doc = fitz.open(file)
     doc.authenticate(pdf_password)
-    page = doc.load_page(3)            
-    text = page.get_text('ocr')
-    # if not text.strip():
-    #     text = page.get_text('ocr')
+    page3 = doc.load_page(4)            
+    pix = page3.get_pixmap()
+    img = Image.open(io.BytesIO(pix.tobytes()))
+    text = pytesseract.image_to_string(img)
     print(text)
