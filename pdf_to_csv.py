ca6e8006c586ffee4b85de3517dbe1439add2261
vashmeow <isanjuanivan@gmail.com>
2025-10-26 02:43:06 +0800

    got the OCR working properly

--- pdf_to_csv.py
+++ pdf_to_csv.py
@@ -8,12 +8,15 @@ import os
 folder_path = '\\Projects\GMAIL_Billing_Extractor\pdf_files'
 pdf_password = pdf_pass()
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
 for page in os.listdir(folder_path):
     file = os.path.join(folder_path,page)
     doc = fitz.open(file)
     doc.authenticate(pdf_password)
-    page3 = doc.load_page(4)            
-    pix = page3.get_pixmap()
+    page3 = doc.load_page(4)
+    zoom = 3
+    mat = fitz.Matrix(zoom, zoom)
+    pix = page3.get_pixmap(matrix=mat)
     img = Image.open(io.BytesIO(pix.tobytes()))
     text = pytesseract.image_to_string(img)
     print(text)
